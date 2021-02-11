56da77d82beaf3348f756f70687dc13aaa9e728f
piotrmonsiorski <piotr.monsiorski@signia.group>
2021-02-11 15:55:05 +0100

    Updates

--- src/api/index.js
+++
@@ -1,8 +0,0 @@
-import axios from 'axios';
-
-const url = 'https://memories-project-pm.herokuapp.com/posts';
-
-export const fetchPosts = () => axios.get(url);
-export const createPost = newPost => axios.post(url, newPost);
-export const updatePost = (id, post) => axios.patch(`${url}/${id}`, post);
-export const deletePost = id => axios.delete(`${url}/${id}`);
--- src/redux/actions/index.js
+++
@@ -1,6 +0,0 @@
-export const actionTypes = {
-  FETCH_ALL: 'FETCH_ALL',
-  CREATE: 'CREATE',
-  UPDATE: 'UPDATE',
-  DELETE: 'DELETE',
-};
